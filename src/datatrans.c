63849e481e30699e54516ee02d2861acb81df513
Steven Arnow <s@rdw.se>
2014-05-13 10:11:07 +0200

    Timeout

--- src/datatrans.c
+++ src/datatrans.c
@@ -14,7 +14,7 @@ void trans_get_mic_data() {
 	timeout = global_timer + 40000;
 
 	while (global_timer < timeout) {
-		radiolink_recv(16, buff);
+		radiolink_recv_timeout(16, buff, 2000);
 		uart_send_raw(buff, 16);
 	}
 
